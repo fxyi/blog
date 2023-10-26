eaf77e4eccd87314603ec41f982e8967933027d4
fxyi <fangxinyii@163.com>
2023-10-27 00:30:23 +0800

    build

--- my/.vitepress/config.mjs
+++ my/.vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/blog/',
   outDir: '../dist',
   title: "笔记",
   description: "A VitePress Site",
